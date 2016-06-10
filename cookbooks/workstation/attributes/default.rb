492423847bb440a799d57b7e10415306a09b7b8e
David Pell <davidcpell@gmail.com>
2016-06-09 21:43:59 -0400

    set up oh-my-zsh and dotfiles

--- cookbooks/workstation/attributes/default.rb
+++ cookbooks/workstation/attributes/default.rb
@@ -1,8 +1,16 @@
+home = Dir.home("david")
+
 default["user"] = {
-  "home" => Dir.home("david"),
+  "home" => home,
+  "code"     => File.join(home, "code"),
+  "dotfiles" => File.join(home, "code", "dotfiles")
 }
 
 default["oh-my-zsh"] = {
   "repo" => "https://github.com/robbyrussell/oh-my-zsh.git"
+}
+
+default["dotfiles"] = {
+  "repo" => "https://github.com/davidcpell/dotfiles.git"
 }
 
--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -4,11 +4,28 @@ end
 
 package "zsh"
 
+directory node["user"]["code"] do
+  user "david"
+end
+
 git File.join(node["user"]["home"], ".oh-my-zsh") do
   user       "david"
-  repository node["oh-my-zsh"]["repo"]
+  repository  node["oh-my-zsh"]["repo"]
   reference  "master"
   action     :sync
+end
+
+git node["user"]["dotfiles"] do 
+  user       "david"
+  repository node["dotfiles"]["repo"]
+  reference  "master"
+  action     :sync
+end
+
+link File.join(node["user"]["home"], ".zshrc") do
+  to   File.join(node["user"]["dotfiles"], "zshrc")
+  user "david"
+  link_type :symbolic
 end
 
 
