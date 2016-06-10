a9da0ecd4eb3e284688c6ed37045c46dbea50248
David Pell <davidcpell@gmail.com>
2016-06-09 20:44:50 -0400

    add oh-my-zsh logic to workstation ckbk

---
+++ cookbooks/workstation/attributes/default.rb
@@ -0,0 +1,8 @@
+default["user"] = {
+  "home" => Dir.home("david"),
+}
+
+default["oh-my-zsh"] = {
+  "repo" => "https://github.com/robbyrussell/oh-my-zsh.git"
+}
+
--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -2,5 +2,14 @@ user "david" do
   action :create
 end
 
+package "zsh"
+
+git File.join(node["user"]["home"], ".oh-my-zsh") do
+  user       "david"
+  repository node["oh-my-zsh"]["repo"]
+  reference  "master"
+  action     :sync
+end
+
 
 
