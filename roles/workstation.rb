649607ed36787706233cea8b9bee75c9836bf470
David Pell <davidcpell@gmail.com>
2016-08-12 20:54:00 -0400

    use the workstation role in kitchen.yml

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,5 +1,10 @@
 name        'workstation'
 description 'role for my development workstations'
 run_list    'recipe[workstation::default]',
-            'recipe[workstation::tmux]'
+            'recipe[workstation::tmux]',
+            'recipe[workstation::dotfiles]',
+            'recipe[workstation::vim]',
+            'recipe[ruby::rbenv]',
+            'recipe[ruby::ruby-build]',
+            'recipe[ruby]'
 
